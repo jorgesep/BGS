1d6fb4d7a382bd57d58fda01676b05ecacafc29b
Jorge Sepulveda <jorge_a_sepulveda@yahoo.com>
2013-08-10 16:16:55 -0400

    Added new uCV BGS module

---
+++ UCV_GMM/src/testUCV.cpp
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * This file is part of libraries to evaluate performance of Background 
+ * Subtraction algorithms.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+
+#include <boost/filesystem.hpp>
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+//#include "BGSFramework.h"
+//#include "MOGBuilder.h"
+//#include "MOG2Builder.h"
+//#include "NPBuilder.h"
+//#include "SAGMMBuilder.h"
+//#include "FrameReaderFactory.h"
+//
+//#include "Performance.h"
+#include "utils.h"
+//#include "NPBGSubtractor.h"
+//#include "mdgkt_filter.h"
+
+#include "ucv_types.h"
+
+
+
+using namespace cv;
+using namespace std;
+using namespace boost::filesystem;
+//using namespace seq;
+//using namespace bgs;
+
+
+/*
+void create_g_img(ucv_image_t *a, uint8_t *a_b, uint16_t w, uint16_t h)
+{
+     a->image = a_b;
+      a->type = UCV_GRAY8U;
+       a->width = w;
+        a->height = h;
+}
+*/
+
+
+int main( int argc, char** argv )
+{
+
+    //uint32_t tmp = UCV_IMAGE_WIDTH(*im) * UCV_IMAGE_HEIGHT(*im) * ng;
+    return 0;
+}
+
