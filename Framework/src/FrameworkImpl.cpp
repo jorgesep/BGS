fa9ebc28b6d2f9acbf387c446a884d5622893185
Jorge Sepulveda <jorge_a_sepulveda@yahoo.com>
2013-08-10 16:08:20 -0400

    Added xml configuration file

--- Framework/src/FrameworkImpl.cpp
+++ Framework/src/FrameworkImpl.cpp
@@ -31,7 +31,7 @@
 #include "FrameReaderFactory.h"
 
 //#include "Performance.h"
-//#include "utils.h"
+#include "utils.h"
 //#include "NPBGSubtractor.h"
 //#include "mdgkt_filter.h"
 
@@ -91,37 +91,63 @@ int main( int argc, char** argv )
         input_frame = FrameReaderFactory::create_frame_reader(inputName);
     } catch (...) {
         cout << "Invalid file name "<< endl;
-        return -1;
+        return 0;
     }
+    
+    // Checking out config file available
+    string configfile = "config/Framework.xml";
+    
+    if ( !exists(configfile) && !is_regular_file(configfile) ) {
+        cout << "File " << configfile << " not found." << endl;
+        return 0;
+    }
+        
+    // Load configuration file
+    FileStorage fs(configfile, FileStorage::READ);
+    
+    bool algorithmMOG2Enabled     = (int)fs["MOG2"];
+    bool algorithmNPEnabled       = (int)fs["NP"];
+    bool algorithmSAGMMEnabled    = (int)fs["SAGMM"];
+    int  InitFGMaskFrame          = (int)fs["InitFGMaskFrame"];
+    int  EndFGMaskFrame           = (int)fs["EndFGMaskFrame"];
+    
+    fs.release();
+    
     
     // Create display windows
     if (displayImages) {
         namedWindow("Current Frame", CV_WINDOW_NORMAL);
         moveWindow("Current Frame", 20, 20);
 
-        namedWindow("MOG2", CV_WINDOW_NORMAL);
-        moveWindow("MOG2", 300, 20);
-
-        namedWindow("Non Parametric", CV_WINDOW_NORMAL);
-        moveWindow("Non Parametric", 200, 20);
-
-        namedWindow("SAGMM", CV_WINDOW_NORMAL);
-        moveWindow("SAGMM", 200, 200);
+        if (algorithmMOG2Enabled){
+            namedWindow("MOG2", CV_WINDOW_NORMAL);
+            moveWindow("MOG2", 300, 20);
+        }
+        
+        if (algorithmNPEnabled) {
+            namedWindow("Non Parametric", CV_WINDOW_NORMAL);
+            moveWindow("Non Parametric", 200, 20);
+        }
+        
+        if (algorithmSAGMMEnabled) {
+            namedWindow("SAGMM", CV_WINDOW_NORMAL);
+            moveWindow("SAGMM", 200, 200);
+        }
     }
     
 
-    int col = input_frame->getNumberCols(); 
-    int row = input_frame->getNumberRows();
-    int nch = input_frame->getNChannels();
+    // Instances of algorithms 
+
+    int col   = input_frame->getNumberCols(); 
+    int row   = input_frame->getNumberRows();
+    int nch   = input_frame->getNChannels();
     int delay = input_frame->getFrameDelay();
     
-    // Creates algorithm objects    
     Framework* mog2 = new Framework();
     mog2->setAlgorithm(new MOG2Builder());
     mog2->setName("MOG2");
     mog2->loadConfigParameters();
     mog2->initializeAlgorithm();
-    
 
     Framework* np = new Framework();
     np->setAlgorithm(new NPBuilder(col,row,nch));
@@ -135,6 +161,43 @@ int main( int argc, char** argv )
     sagmm->loadConfigParameters();
     sagmm->initializeAlgorithm();
     
+    
+    
+    // Prapare directory masks
+    string np_foreground_path    = "np_mask";
+    string sagmm_foreground_path = "sagmm_mask";
+    string mog2_foreground_path  = "mog2_mask";
+    
+    if (saveForegroundMask) {
+
+        // Create directoty if not exists and create a numbered internal directory.
+        // np_mask/0, np_mask/1, ...
+        bgs::create_foreground_directory(np_foreground_path);
+        bgs::create_foreground_directory(sagmm_foreground_path);
+        bgs::create_foreground_directory(mog2_foreground_path);
+
+        ofstream outfile;
+        stringstream param;
+        param << np_foreground_path << "/parameters.txt" ;
+        outfile.open(param.str().c_str());
+        outfile << np->getConfigurationParameters();
+        outfile.close();
+        
+        param.str("");
+        param << sagmm_foreground_path << "/parameters.txt" ;
+        outfile.open(param.str().c_str());
+        outfile << sagmm->getConfigurationParameters();
+        outfile.close();
+
+        param.str("");
+        param << mog2_foreground_path << "/parameters.txt" ;
+        outfile.open(param.str().c_str());
+        outfile << mog2->getConfigurationParameters();
+        outfile.close();
+
+    }
+    
+   
      
     Mat Frame;
     Mat Image;
@@ -156,16 +219,58 @@ int main( int argc, char** argv )
         
         if (Frame.empty()) break;
     
-        mog2->updateAlgorithm(Frame, Foreground);
-        np->updateAlgorithm(Frame, NPMask);
-        sagmm->updateAlgorithm(Frame, SAMask);
+        if (algorithmMOG2Enabled) {
+            mog2->updateAlgorithm(Frame, Foreground);
+            
+            if (displayImages)
+                imshow("MOG2", Foreground);
+        }
+        
+        if (algorithmNPEnabled) {
+            np->updateAlgorithm(Frame, NPMask);
+            
+            if (displayImages)
+                imshow("Non Parametric", NPMask);
+        }
+        
+        if (algorithmSAGMMEnabled) {
+            sagmm->updateAlgorithm(Frame, SAMask);
+            
+            if (displayImages)
+                imshow("SAGMM", SAMask);
+        }
+        
+        // Save foreground images
+        if (saveForegroundMask && cnt >= InitFGMaskFrame && cnt <= EndFGMaskFrame) {
+            stringstream str;
+            vector<int> compression_params;
+            compression_params.push_back(CV_IMWRITE_JPEG_QUALITY);
+            compression_params.push_back(100);
+
+            if (algorithmMOG2Enabled) {
+                str << mog2_foreground_path << "/" <<  cnt << ".jpg";
+                imwrite(str.str(), Foreground, compression_params);
+            }
+            if (algorithmNPEnabled) {
+                str.str("");
+                str << np_foreground_path << "/" <<  cnt << ".jpg";
+                imwrite(str.str(), NPMask, compression_params);
+                
+            }
+            if (algorithmSAGMMEnabled) {
+                str << sagmm_foreground_path << "/" <<  cnt << ".jpg";
+                imwrite(str.str(), SAMask, compression_params);
+                
+            }
+            
+        }
+        
+
+        
         
         if (displayImages) {
             Frame.convertTo(Image, CV_8UC3);
             imshow("Current Frame", Image);
-            imshow("MOG2", Foreground);
-            imshow("Non Parametric", NPMask);
-            imshow("SAGMM", SAMask);
             
             char key=0;
             key = (char)waitKey(delay);
