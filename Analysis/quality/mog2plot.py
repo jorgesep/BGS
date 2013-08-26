d5c3ea0b6a56697d3b3e24033cdb863e13a7220c
Jorge Sepulveda <jorge_a_sepulveda@yahoo.com>
2013-08-26 01:30:50 -0400

    Added python script to plot mog2  and generics

---
+++ Analysis/quality/mog2plot.py
@@ -0,0 +1,173 @@
+#!/usr/bin/python
+
+from optparse import OptionParser
+import sys
+import re
+import os
+
+import pylab as pl
+from math import *
+
+import numpy as np
+from scipy.integrate import trapz
+
+from rocplot import plotroc
+
+   
+    
+
+        
+class plotmog2(plotroc) :
+
+    def __init__(self, indexes=None, internal_range=None, one_value=None, title=None, label=None):
+        plotroc.__init__(self, indexes, internal_range, one_value)
+
+        self.title = title
+        if title == None:
+            self.title = 'ROC Curve MOG2 Algorithm'
+
+        self.label = label
+        if label == None:
+            self.label = 'MuHAVI-MAS'
+        self._set_title(self.title)
+        self._set_text (self.label)
+
+    def plot1(self):
+        self._generic_plot(self.name_0, self.name_1, 'MOG2_TPR_FPR1.png')
+ 
+    def plot2(self):
+        self._generic_plot(self.name_1, self.name_0, 'MOG2_TPR_FPR2.png')
+
+    def plot3(self):
+        self._plot_range(self.name_0, self.name_1 , 'MOG2_TPR_FPR3.png')
+
+    def plot4(self):
+        self._plot_one_line(self.name_0, self.name_1 , 'MOG2_TPR_FPR4.png', self.parameter_value)
+
+    def plot5(self):
+        self._plot_mean_and_median('MOG2_TPR_FPR5.png', self.parameter_value)
+
+    def plot6(self):
+        self._plot_mean_and_median_same_graph('MOG2_TPR_FPR6.png', self.parameter_value)
+
+
+
+def update_file(filename, col,val):
+    f     = open(filename, 'r')
+    lines = f.readlines()
+    f.close()
+
+    values = [i.strip() for i in lines[0].split(';')]
+    values[col] = str(val)
+
+    f     = open("mog2_labels.txt", 'w')
+    line="; ".join(values)
+    print >> f,line
+    f.close()
+
+ 
+
+#############
+        
+if __name__ == '__main__':
+
+    parser = OptionParser ()
+    parser.add_option ("-f", "--file",dest = "file",
+                       type = "string",
+                       default = None,
+                       help = "Select input file, e.g -f final_measures.txt")
+    parser.add_option ("-c", "--columns",dest = "columns",
+                       type = "string",
+                       default = None,
+                       help = "Select two colums to be processed, e.g -c 'Alpha,Threshold'")
+    parser.add_option ("-r", "--range",dest = "range",
+                       type = "string",
+                       default = None,
+                       help = "Plot selected range, e.g -r '0.01-0.03' or -r '0.01 0.03 0.06' ")
+    parser.add_option ("-v", "--value",dest = "value",
+                       type = "string",
+                       default = None,
+                       help = "Plot single value, e.g -v '0.03'")
+    parser.add_option ("-t", "--title",dest = "title",
+                       type = "string",
+                       default = None,
+                       help = "Set title of the plot")
+    parser.add_option ("-l", "--label",dest = "label",
+                       type = "string",
+                       default = None,
+                       help = "Set bottom label text in the plot")
+    parser.add_option ("-s", "--sequence_name",dest = "sequence_name",
+                       type = "string",
+                       default = None,
+                       help = "Set name of sequence to processed")
+
+    (options, args) = parser.parse_args ()
+
+    if options.file == None :
+        parser.print_help()
+        sys.exit()
+ 
+    ##########
+    #Variables
+    #########
+
+    val   = options.value
+    cols   = options.columns
+    r4nge   = options.range
+    title = options.title
+    text  = options.label
+
+    # Setting title for plots
+    if options.title == None:
+        if os.path.exists("mog2_labels.txt"):
+            f = open('mog2_labels.txt', 'r')
+            lines = f.readlines()
+            f.close()
+
+            title = [i.strip() for i in lines[0].split(';')][1]
+            text  = [i.strip() for i in lines[0].split(';')][2]
+            cols  = [i.strip() for i in lines[0].split(';')][3]
+            if cols == "" :  cols = None
+            r4nge = [i.strip() for i in lines[0].split(';')][4]
+            if r4nge == "" :  r4nge = None
+            val   = [i.strip() for i in lines[0].split(';')][5]
+            if val == "" :  val   = None
+
+        else :
+            # Creating file
+            title = "ROC Curve MOG2 Algorithm"
+            text  = "MuHAVI-MAS"
+            seq   = "<Sequence Name>"
+            cols  = 'LearningRate,Threshold'
+            r4nge = '0.001-0.003'
+            val   = '0.001'
+            f     = open("mog2_labels.txt", 'w')
+            line="1;%s ;%s\n%s ; %s ; %s ; %s" % (title,text, seq, cols, r4nge, val)
+            print >> f,line
+            f.close()
+
+
+   
+    if options.value != None :
+        # Save them in file
+        val   = options.value
+        update_file('mog2_labels.txt',5,val)
+
+    if options.range != None :
+        # Save them in file
+        r4nge   = options.range
+        update_file('mog2_labels.txt',4,r4nge)
+
+    if options.columns != None :
+        # Save them in file
+        cols   = options.columns
+        update_file('mog2_labels.txt',3,cols)
+
+    n = plotmog2(cols,r4nge, val, title, text)
+    n.load(options.file)
+    n.plot1()
+    n.plot2()
+    n.plot3()
+    n.plot4()
+    n.plot5()
+    n.plot6()
